8c81cff01c36ec51f919b8a99710f9c7d0c909dc
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-08-11 22:53:59 +0800

    component menu

---
+++ resources/views/components/menu-link.blade.php
@@ -0,0 +1,7 @@
+<li>
+    <a href="{{ is_null($routeName ?? null) ? '#' : route($routeName) }}"
+        class="group flex flex-col items-center text-neutral-600 justify-center gap-1 hover:text-rose-500 hover:bg-rose-50 p-2 {{ request()->segment(1) == $segment ? 'active-menu' : '' }}">
+        {{ $icon }}
+        <span class="block group-hover:text-rose-500 text-sm">{{ $label }}</span>
+    </a>
+</li>
