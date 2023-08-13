abbd7ad3292ce40ab84eda37f7f2d3be92aa5a23
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-08-13 21:54:13 +0800

    make component for button back to home

---
+++ resources/views/components/btn-back-home.blade.php
@@ -0,0 +1,5 @@
+<a href="{{ route('home') }}" type="button"
+    class="border-2 border-slate-400 hover:bg-slate-800 hover:border-white hover:text-white transition duration-150 focus:ring-4 unmobile:focus:ring-2 focus:outline-none focus:ring-slate-900 rounded-full font-medium inline-flex items-center justify-between px-6 py-3 unmobile:text-sm unmobile:px-4 unmobile:py-2">
+    <x-icon-arrow-left-bold class="unmobile:h-5 unmobile:w-5 h-6 w-6 mr-2" />
+    <span class="flex-1 text-md mr-2">Kembali</span>
+</a>
