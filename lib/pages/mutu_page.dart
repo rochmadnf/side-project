c9ca5bc1598bb55e43fc78de5730d7c7529755a9
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-09-23 15:27:08 +0800

    done #1

--- lib/pages/mutu_page.dart
+++ lib/pages/mutu_page.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
 import 'package:bamer_palu/components/subtitle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:dropdown_textfield/dropdown_textfield.dart';
+import 'package:smart_alert_dialog/smart_alert_dialog.dart';
+import 'package:smart_alert_dialog/models/alert_dialog_text.dart';
 
 class MutuPage extends StatefulWidget {
   const MutuPage({super.key});
@@ -13,19 +17,72 @@ class MutuPage extends StatefulWidget {
 
 class _MutuPageState extends State<MutuPage> {
   // @GlobalVariable
-  int valueKetuaan = 1;
+  int valueKetuaan = 1,
+      valueKekerasan = 0,
+      valueKebusukan = 0,
+      valueKerusakan = 0,
+      powerUp = 2;
   double valueKadarAir = 0, valueDiameter = 0;
 
   void calculate() {
-    print(valueKetuaan);
-    print(valueKadarAir);
+    valueKetuaan = (valueKetuaan < 60) ? 0 : 1;
+    valueKadarAir = (valueKadarAir >= 80 && valueKadarAir <= 85) ? 1 : 0;
+
+    // diameter
+    if (valueDiameter > 2.5) {
+      valueDiameter = 1;
+    } else if (valueDiameter >= 1.5 && valueDiameter <= 2.5) {
+      valueDiameter = 0.5;
+    } else {
+      valueDiameter = 0;
+    }
+
+    // proses cek logaritma
+    dynamic mutu1 = (pow((0.9484 - valueKetuaan), powerUp)) +
+        (pow((0.9992 - valueKekerasan), powerUp)) +
+        (pow((0.0069 - valueKebusukan), powerUp)) +
+        (pow((0.5276 - valueKerusakan), powerUp)) +
+        (pow((0.8617 - valueKadarAir), powerUp)) +
+        (pow((0.5300 - valueDiameter), powerUp));
+
+    dynamic mutu2 = (pow((0.9688 - valueKetuaan), powerUp)) +
+        (pow((0.0000 - valueKekerasan), powerUp)) +
+        (pow((0.0000 - valueKebusukan), powerUp)) +
+        (pow((0.0000 - valueKerusakan), powerUp)) +
+        (pow((0.0625 - valueKadarAir), powerUp)) +
+        (pow((0.4844 - valueDiameter), powerUp));
+
+    //  1 0 0 0 1 0.5 || 1.33 || 0.88 || C2
+    print(mutu1);
+    print(mutu2);
+    if (mutu1 < mutu2) {
+      _okSmartAlert(context, "Bawang Merah Mutu I");
+    } else {
+      _okSmartAlert(context, "Bawang Merah Mutu II");
+    }
+  }
+
+  void _okSmartAlert(BuildContext context, String text) {
+    showDialog(
+      context: context,
+      builder: (_) => SmartAlertDialog(
+        title: "Hasil Pengecekan",
+        text: AlertDialogText(
+          confirm: '',
+          cancel: '',
+          dismiss: 'OK',
+        ),
+        message: text,
+        isDismissible: true,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
             Subtitle(label: 'Uji Data'),
@@ -117,6 +174,151 @@ class _MutuPageState extends State<MutuPage> {
                   const SizedBox(
                     height: 15.0,
                   ),
+
+                  // @Kekerasan
+                  Text(
+                    'Bagaimana tekstur kekerasan Bawang Merah?',
+                    style: GoogleFonts.poppins(fontSize: 14.0),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+
+                  //@InputKekerasan
+                  DropDownTextField(
+                    initialValue: "Keras",
+                    onChanged: (val) {
+                      valueKekerasan = val.value;
+                    },
+                    textFieldDecoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 10),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: HexColor('#f43f5e'),
+                        ),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      hintText: "--Pilih Kekerasan--",
+                    ),
+                    dropdownRadius: 4.0,
+                    enableSearch: false,
+                    clearOption: false,
+                    dropDownIconProperty: IconProperty(
+                      color: HexColor('#f43f5e'),
+                    ),
+                    dropDownList: const [
+                      DropDownValueModel(name: 'Keras', value: 0),
+                      DropDownValueModel(name: 'Cukup Keras', value: 1),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+
+                  // @Kebusukan
+                  Text(
+                    'Kondisi Bawang Merah busuk?',
+                    style: GoogleFonts.poppins(fontSize: 14.0),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+
+                  //@InputKekerasan
+                  DropDownTextField(
+                    initialValue: "Tidak",
+                    onChanged: (val) {
+                      valueKebusukan = val.value;
+                    },
+                    textFieldDecoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 10),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: HexColor('#f43f5e'),
+                        ),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      hintText: "--Pilih Kebusukan--",
+                    ),
+                    dropdownRadius: 4.0,
+                    enableSearch: false,
+                    clearOption: false,
+                    dropDownIconProperty: IconProperty(
+                      color: HexColor('#f43f5e'),
+                    ),
+                    dropDownList: const [
+                      DropDownValueModel(name: 'Ya', value: 1),
+                      DropDownValueModel(name: 'Tidak', value: 0),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+
+                  // @Kerusakan
+                  Text(
+                    'Kondisi Bawang Merah rusak?',
+                    style: GoogleFonts.poppins(fontSize: 14.0),
+                  ),
+                  const SizedBox(
+                    height: 5.0,
+                  ),
+
+                  //@InputKekerasan
+                  DropDownTextField(
+                    initialValue: "Tidak",
+                    onChanged: (val) {
+                      valueKerusakan = val.value;
+                    },
+                    textFieldDecoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 5.0, horizontal: 10),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: HexColor('#f43f5e'),
+                        ),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      hintText: "--Pilih Kebusukan--",
+                    ),
+                    dropdownRadius: 4.0,
+                    enableSearch: false,
+                    clearOption: false,
+                    dropDownIconProperty: IconProperty(
+                      color: HexColor('#f43f5e'),
+                    ),
+                    dropDownList: const [
+                      DropDownValueModel(name: 'Ya', value: 1),
+                      DropDownValueModel(name: 'Tidak', value: 0),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  // @EndKerusakan
 
                   // @KadarAir
                   Text(
